21d7446c8ef147193656e0682020a375d43dd004
Angel Gonzalez <aangel.gg@hotmail.com>
2025-07-01 20:13:17 +0200

    Small fixes

--- src/components/PositionDetailsComponent/PositionInfoDetailsComponent.tsx
+++ src/components/PositionDetailsComponent/PositionInfoDetailsComponent.tsx
@@ -24,7 +24,7 @@ const PositionInfoDetailsComponent = (props: PositionInfoDetailsComponentProps) 
 					<Span>{categoryName}</Span>
 					{subcategoryName && (
 						<SeparatorComponent>
-							<Span>{subcategoryName}</Span> : null
+							<Span>{subcategoryName}</Span>
 						</SeparatorComponent>
 					)}
 					<>
