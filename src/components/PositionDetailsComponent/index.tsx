f83b8390cb12f2f9ccedf97e7e69661f10b8744e
Angel Gonzalez <aangel.gg@hotmail.com>
2025-07-01 16:50:58 +0200

    Fix referral bonus

--- src/components/PositionDetailsComponent/index.tsx
+++ src/components/PositionDetailsComponent/index.tsx
@@ -7,9 +7,12 @@ interface Props {
 	project: Project;
 }
 const ProjectDetailsComponent = (props: Props) => {
+	const { project } = props;
+
+	const referralBonus = project.positions[0].referralBonus;
 	return (
 		<Stack gap="8px" className="slide-in-bounce">
-			<ReferralBonusComponent referralBonus={300} />
+			{referralBonus && <ReferralBonusComponent referralBonus={referralBonus} />}
 			<ProjectInformationComponent project={props.project} />
 		</Stack>
 	);
